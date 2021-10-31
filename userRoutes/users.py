5440546bea5d38f78a0809b7b5ffc8c976edcc39
Adrian Unruh <unruha@uw.edu>
2021-10-31 14:48:27 -0700

    user api takes params

--- userRoutes/users.py
+++ userRoutes/users.py
@@ -1,11 +1,14 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 from database import mongo_client
 
 userRoutes = Blueprint('userRoutes', __name__)
 
-@userRoutes.route('/user/<string:name>', methods=['POST'])
-def user(name):
+@userRoutes.route('/user', methods=['POST'])
+def user():
     user_collection = mongo_client.db.Users
-    user_collection.insert_one({'Name': name, 'Username': "lmao"})
+    email = request.args.get('email')
+    id = request.args.get('id')
+    user_collection.insert_one({'email': email, 'id': id})
     return jsonify(message="success")
